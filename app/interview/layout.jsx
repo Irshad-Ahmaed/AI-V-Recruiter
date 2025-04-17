a9d4738dd2d92da814b730e8cfa37b0a09812fdb
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-04-17 23:08:55 +0530

    Added auth check in interview page

--- app/interview/layout.jsx
+++ app/interview/layout.jsx
@@ -1,27 +1,34 @@
-"use client"
-import React, { useEffect } from 'react'
+"use client";
+import React, { useEffect } from 'react';
 import InterviewHeader from './_components/InterviewHeader';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import { useUser } from '../provider';
+import { Loader2Icon } from 'lucide-react';
 
-const InterviewLayout = ({children}) => {
-  const { user } = useUser();
+const InterviewLayout = ({ children }) => {
+  const { user, loading } = useUser();
   const router = useRouter();
 
   useEffect(() => {
+    if (loading) return;
+
     if (!user) {
       toast.error("Please Login");
       router.push('/auth');
     }
-  }, [user]);
-  
+  }, [user, loading, router]);
+
+  if (loading) {
+    return <div className="p-10"><Loader2Icon className='size-4 space-x-2 animate-spin' /> Loading...</div>;
+  }
+
   return (
     <div className='bg-secondary pb-6'>
-        <InterviewHeader/>
-        {children}
+      <InterviewHeader />
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default InterviewLayout
+export default InterviewLayout;
